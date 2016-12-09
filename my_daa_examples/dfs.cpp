0c753d44538d1bd9125dfebb03ff54b4a4f7d66a
sudnya <mailsudnya@gmail.com>
2016-12-09 15:35:02 -0800

    ADD: dfs c++14

---
+++ my_daa_examples/dfs.cpp
@@ -0,0 +1,100 @@
+/*
+ *
+ * \file    dfs.cpp
+ * \author  Sudnya Diamos <mailsudnya@gmail.com>
+ * \date    Friday Dec 9, 2016
+ * \brief   breadth first search
+ *
+**/
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <stack>
+
+template <typename T>
+void printSet(const T v)
+{
+    for (auto& i : v)
+        std::cout << i << " , ";
+    std::cout << "\n";
+}
+
+template <typename T>
+void runDfs(T g) 
+{
+    std::set<int> visited;
+    std::stack<int> frontier;
+    
+    frontier.push(0);
+    visited.insert(0);
+
+    while(!frontier.empty() && visited.size() != g.size() )
+    {
+        //get adjacent
+        auto current = frontier.top();
+        std::cout << "visiting current node: " << current << std::endl;
+        frontier.pop();
+        
+        visited.insert(current);
+
+        auto adjacent = g.at(current);
+
+        for (auto i = adjacent.begin(); i != adjacent.end(); ++i)
+        {
+            bool isVisited = visited.find(*i) != visited.end();
+            if (!isVisited)
+            {
+                std::cout << "adding " << *i << " to frontier\n";
+                frontier.push(*i);
+            }
+        }
+        //std::cout << "frontier: ";
+        //printSet(frontier);
+        //std::cout << "visited: ";
+        //printSet(visited);
+    }
+}
+
+template <typename T>
+void printInput(T g)
+{
+    int counter = 0;
+    for (auto i = g.begin(); i != g.end(); ++i, ++counter)
+    {
+        std::cout << "\nNode: " << counter << " : ";
+        for (auto j = i->begin(); j != i->end(); ++j)
+        {
+            std::cout << *j << " \t ";
+        }
+    }
+
+}
+/*
+ *      0 -> 1 ->   3
+ *     \|  .  /.\  \|/
+ * 5 <- 2 \./     .4
+ *
+ */
+int main()
+{
+    std::cout << "DFS\n";
+    //create a graph
+    //0-> 1, 2
+    //1-> 2, 3
+    //2-> 5
+    //3-> 4
+    //4-> 1
+    std::vector<int>zero  = {1,2};
+    std::vector<int>one   = {2,3};
+    std::vector<int>two   = {5};
+    std::vector<int>three = {4};
+    std::vector<int>four  = {1};
+    std::vector<int>five  = {};
+
+    std::vector<std::vector<int>> g = {zero, one, two, three, four, five};
+
+    //printInput(g);
+
+    runDfs(g);
+}
